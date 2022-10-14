b2de8c57e442d3cc5190a945c7f912a9a4c97ff2
Kayo Ronald <kayoronald4321@gmail.com>
2022-10-14 11:30:59 +0000

    fix: corrigindo falhas

--- app/schema/schemas.py
+++ app/schema/schemas.py
@@ -1,8 +1,9 @@
 from typing import Union, Optional
 from pydantic import BaseModel
 
+
 class Produto(BaseModel):
-  id: Optional[int]
+  id: Optional[str] = None
   nome: str
   descricao: Union[str, None] = None
   valor: float
